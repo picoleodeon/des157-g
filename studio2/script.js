9893a9293beb457ea5a733c6a23f6c33fda33166
Nicole Arteaga <narteaga@scc2102-21.ucdavis.edu>
2019-02-04 11:38:42 -0800

    init

---
+++ studio2/script.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('reading js');
+
+var backPack=document.querySelector('#backpack');
+var pocket1=document.querySelector('#pocket1');
+var pocket2=document.querySelector('#pocket2');
+var flap=document.querySelector('#flap');
+
+ backPack.addEventListener('click', function(){
+  paragraph.innerHTML='stuff+about+chapstick';
+});
