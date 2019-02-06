0fc5af7e232fcc25ac10064b3b0c90bca422fd68
picoleodeon <46665449+picoleodeon@users.noreply.github.com>
2019-02-06 11:56:12 -0800

    init

--- studio2/script.js
+++ studio2/script.js
@@ -1,11 +1,12 @@
 'use strict';
 console.log('reading js');
 
-var backPack=document.querySelector('#backpack');
-var pocket1=document.querySelector('#pocket1');
-var pocket2=document.querySelector('#pocket2');
-var flap=document.querySelector('#flap');
+var topSpot=document.querySelector('#topSpot');
+var rightSpot=document.querySelector('#rightSpot');
+var leftSpot=document.querySelector('#leftSpot');
+var backPack=document.querySelector('#backPack');
+var overlay = document.querySelector('#overlay');
 
- backPack.addEventListener('click', function(){
-  paragraph.innerHTML='stuff+about+chapstick';
-});
+ topSpot.addEventListener('click', function(){
+   overlay.style.display='block';
+ },false);
