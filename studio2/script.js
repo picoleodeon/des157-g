f19daaf91c10b2ffd336f6163a16d46eb88decce
picoleodeon <46665449+picoleodeon@users.noreply.github.com>
2019-02-10 23:58:49 -0800

    init

--- studio2/script.js
+++ studio2/script.js
@@ -1,12 +1,34 @@
 'use strict';
 console.log('reading js');
+var closeOne=document.querySelector('#closeOne');
+var closeTwo=document.querySelector('#closeTwo');
+var closeThree=document.querySelector('#closeThree');
 
 var topSpot=document.querySelector('#topSpot');
 var rightSpot=document.querySelector('#rightSpot');
 var leftSpot=document.querySelector('#leftSpot');
-var backPack=document.querySelector('#backPack');
-var overlay = document.querySelector('#overlay');
 
- topSpot.addEventListener('click', function(){
-   overlay.style.display='block';
- },false);
+var overlay = document.querySelector('#overlay');
+var overlayTwo = document.querySelector('#overlayTwo');
+var overlayThree = document.querySelector('#overlayThree');
+
+topSpot.addEventListener('click', function() {
+  overlay.style.display = 'block';
+}, false);
+closeOne.addEventListener('click', function() {
+  overlay.style.display = 'none';
+}, false);
+
+leftSpot.addEventListener('click', function() {
+  overlayTwo.style.display = 'block';
+}, false);
+closeTwo.addEventListener('click', function() {
+  overlayTwo.style.display = 'none';
+}, false);
+
+rightSpot.addEventListener('click', function() {
+  overlayThree.style.display = 'block';
+}, false);
+closeThree.addEventListener('click', function() {
+  overlayThree.style.display = 'none';
+}, false);
